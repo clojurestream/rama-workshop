ea41ab06daf2d7ef0c5dba9a34d4b6aed77ef20a
Nathan Marz <nathan@redplanetlabs.com>
2026-06-03 08:24:33 -1000

    add comment to help PState exercise

--- src/workshop/pstate.clj
+++ src/workshop/pstate.clj
@@ -8,3 +8,14 @@
 (defn test-pstate
   []
   (rtest/create-test-pstate {String {String [Object]}}))
+
+
+(comment
+  (use 'com.rpl.rama)
+  (use 'com.rpl.rama.path)
+  (require '[com.rpl.rama.test :as rtest])
+  (require '[workshop.pstate :as pstate] :reload)
+  (def p (pstate/test-pstate))
+
+  (rtest/test-pstate-select-one STAY p)
+  )
